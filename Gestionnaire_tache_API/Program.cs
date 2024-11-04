8769cbf3c85c5c0e6562c6f6535fb8b13711e6bc
Mutala Mutala <jonamut12.live@gmail.com>
2024-11-03 20:55:39 -0500

    .

--- Gestionnaire_tache_API/Program.cs
+++ Gestionnaire_tache_API/Program.cs
@@ -5,6 +5,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+
+//Permettre à L'API de recevoir d'autre requetes 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("all", builder => builder.AllowAnyOrigin()
+    .AllowAnyHeader()
+    .AllowAnyMethod());
+});
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddIdentityServices(builder.Configuration); 
