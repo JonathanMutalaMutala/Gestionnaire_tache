4e13d65108c8eb29c2c50b157ca3fa9809063366
Mutala Mutala <jonamut12.live@gmail.com>
2024-11-30 14:07:16 -0500

    Add Model tache UI

--- Gestionnaire_UI/src/app/column-dto-model.ts
+++ Gestionnaire_UI/src/app/column-dto-model.ts
@@ -2,4 +2,13 @@ export interface ColumnDto {
   name: string;
   description?: string
   order: number
+  taches: Tache[]
+}
+
+export interface Tache {
+  title: string
+  description?: string
+  isCompleted: boolean
+  createdAt: Date
+  ColumnId: number
 }
